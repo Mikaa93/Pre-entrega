a78c89bbd7f481f46103cd5dcc1fe59767e1fb55
Mikaa93 <lopezmicaela2693@gmail.com>
2022-08-18 23:09:53 -0300

    version final

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 let productos =[ ];
 let total = 0;
+let precio= 0;
+
 
 function agregar(producto,precio){
     console.log(producto,precio)
     productos.push(producto);
     total = total + precio;
-    document.getElementById("checkaut").innerHTML = 'Pagar $${total}'
+    document.getElementById("checkaut").innerHTML = "Pagar $${total}"
 }
-
 function pagar(){
     console.log(productos,precio)
-window.alert(productos.join(" , \n"));
+window.alert(productos.join(" ,\n " + " \n Total ") + " "+ total);
 }
 
