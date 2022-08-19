3bad414d0c4351ef9ba329cca92a18f88b23e058
Mikaa93 <lopezmicaela2693@gmail.com>
2022-08-18 22:17:07 -0300

    primer repositorio

---
+++ app.js
@@ -0,0 +1,15 @@
+let productos =[ ];
+let total = 0;
+
+function agregar(producto,precio){
+    console.log(producto,precio)
+    productos.push(producto);
+    total = total + precio;
+    document.getElementById("checkaut").innerHTML = 'Pagar $${total}'
+}
+
+function pagar(){
+    console.log(productos,precio)
+window.alert(productos.join(" , \n"));
+}
+
