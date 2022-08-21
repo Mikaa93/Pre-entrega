e42a7e1b147d98407e5b61e138e1d10465e65323
Mikaa93 <lopezmicaela2693@gmail.com>
2022-08-21 19:22:38 -0300

    version modificada

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+//llamo a la funcion para dar bienvenida a la pagina
+
+function solicitarNombre(){
+    let nombre = prompt("Ingresar nombre")
+    alert("Bienvenido a Perfume shop " + nombre)
+
+}
+solicitarNombre();
+
+
+
 let productos =[ ];
 let total = 0;
 let precio= 0;
+
+//esta sirve para agregar los producto al carrito
 
 
 function agregar(producto,precio){
@@ -9,8 +22,14 @@ function agregar(producto,precio){
     total = total + precio;
     document.getElementById("checkaut").innerHTML = "Pagar $${total}"
 }
+agregar(productos,precio);
+
+//llamo a esta funcion para pagar el total
+
+
 function pagar(){
     console.log(productos,precio)
 window.alert(productos.join(" ,\n " + " \n Total ") + " "+ total);
 }
+pagar();
 
