49a15e2949a4d5482ba0de4d4e2d999248683670
Mikaa93 <lopezmicaela2693@gmail.com>
2022-10-03 21:50:16 -0300

    entregable

--- app.js
+++ app.js
@@ -24,13 +24,28 @@ agregar(productos,precio);
 
 //se agrega un evento
 
-let pagar=
-document.getElementById("checkaut")
-pagar.addEventListener("click", respuestacliente)
-function respuestacliente(){
-    console.log("el cliente hizo el pago")
+let pagar = document.getElementById("checkaut")
+pagar.addEventListener("click",respuestacliente)
+function respuestacliente() {
+    swal({
+        title: "Está seguro de su compra?",
+        icon: "info",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                swal("Has realizado la compra correctamente",{
+                    icon: "success",
+                });
+            } else {
+                swal("Vuelve pronto!",{
+                    icon: "info"
+                });
+            }
+        });
+
 };
-respuestacliente();
 
 //llamo a la funcion para pagar el total
 
@@ -47,7 +62,12 @@ const producto = [{id: 1, producto: "Carolina Herrera", precio:5000},
                   {id:2, producto:"Armani", precio: 10000},
                   {id:3, producto: "Chanel", precio: 10000},
                   {id:4, producto: "Acqua Di Gio", precio:12000},
-                  {id:5, producto:"Versace",precio:8000}];
+                  {id:5, producto:"Versace",precio:8000},
+                  {id:6, producto:"Bvlgari",precio:11000},
+                  {id:7, producto:"Paco Rabanne",precio:15000},
+                  {id:8, producto:"Givenchy",precio:18000}
+                
+                ];
 
 const guardarLocal = (listadeproductos,perfumes) => {localStorage.setItem(listadeproductos,perfumes)};
 
@@ -55,3 +75,25 @@ const guardarLocal = (listadeproductos,perfumes) => {localStorage.setItem(listad
 guardarLocal("listasproductos",JSON.stringify(producto));
 
 console.log(...producto)
+
+
+
+const lista = document.querySelector('#listado')
+
+fetch('/data.json')
+.then((res) => res.json())
+.then( (data) => {
+    data.forEach((producto) => {
+        const li = document.createElement('li')
+        li.innerHTML = `
+        <h4>${producto.nombre}</h4>
+        <p>${producto.precio}</p>
+        <p>Codigo: ${producto.id} </p>
+
+        `
+
+        lista.append(li)
+
+
+    })
+})
